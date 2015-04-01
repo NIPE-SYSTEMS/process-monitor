e70e73f59cca603df39e4f3faf37f39533385fa8
Max Mustermann <max.mustermann7643@web.de>
2015-04-01 09:36:53 +0200

    Basic logic and reaction

--- src/process-monitor.c
+++ src/process-monitor.c
@@ -17,11 +17,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <glibtop.h>
 #include <glibtop/proclist.h>
 #include <glibtop/procstate.h>
 #include <glibtop/procuid.h>
+#include <glibtop/procargs.h>
 
 struct process
 {
@@ -31,10 +33,27 @@ struct process
 	struct process *next;
 	struct process *child;
 };
+typedef struct process process_t;
 
-struct process *process_new(pid_t pid, pid_t ppid, char *command)
+typedef enum { TERMINATE } process_monitor_t;
+typedef enum { DO_NOTHING, RESTART } process_monitor_action_t;
+
+typedef struct
 {
-	struct process *proc = malloc(sizeof(struct process));
+	pid_t pid;
+	char *command;
+	process_monitor_t monitor;
+	process_monitor_action_t action;
+	char **args;
+} process_monitor_job_t;
+
+static process_t *process_root = NULL;
+static process_monitor_job_t *process_monitor_job_list = NULL;
+static int process_monitor_job_list_amount = 0;
+
+process_t *process_new(pid_t pid, pid_t ppid, char *command)
+{
+	process_t *proc = malloc(sizeof(process_t));
 	if(proc == NULL)
 	{
 		return NULL;
@@ -49,7 +68,7 @@ struct process *process_new(pid_t pid, pid_t ppid, char *command)
 	return proc;
 }
 
-void process_free(struct process *proc)
+void process_free(process_t *proc)
 {
 	if(proc != NULL)
 	{
@@ -67,9 +86,11 @@ void process_free(struct process *proc)
 	}
 	
 	free(proc);
+	
+	proc = NULL;
 }
 
-void process_print(struct process *proc, int shift)
+void process_print(process_t *proc, int shift)
 {
 	int i = 0;
 	
@@ -80,10 +101,10 @@ void process_print(struct process *proc, int shift)
 	
 	for(i = 0; i < shift; i++)
 	{
-		printf("      ");
+		printf(". ");
 	}
 	
-	printf("%6i\n", proc->pid);
+	printf("%s (%i)\n", proc->command, proc->pid);
 	
 	if(proc->child != NULL)
 	{
@@ -96,7 +117,7 @@ void process_print(struct process *proc, int shift)
 	}
 }
 
-void process_append(struct process *proc0, struct process *proc1)
+void process_append(process_t *proc0, process_t *proc1)
 {
 	if(proc0 == NULL || proc0->next != NULL || proc1 == NULL)
 	{
@@ -106,7 +127,7 @@ void process_append(struct process *proc0, struct process *proc1)
 	proc0->next = proc1;
 }
 
-void process_append_child(struct process *parent, struct process *child)
+void process_append_child(process_t *parent, process_t *child)
 {
 	if(parent == NULL || parent->child != NULL || child == NULL)
 	{
@@ -116,9 +137,9 @@ void process_append_child(struct process *parent, struct process *child)
 	parent->child = child;
 }
 
-struct process *process_last(struct process *first)
+process_t *process_last(process_t *first)
 {
-	struct process *pointer = first;
+	process_t *pointer = first;
 	
 	if(first == NULL)
 	{
@@ -133,7 +154,7 @@ struct process *process_last(struct process *first)
 	return pointer;
 }
 
-struct process *process_last_child(struct process *first_child)
+process_t *process_last_child(process_t *first_child)
 {
 	if(first_child == NULL || first_child->child == NULL)
 	{
@@ -143,10 +164,10 @@ struct process *process_last_child(struct process *first_child)
 	return process_last(first_child->child);
 }
 
-int process_count(struct process *proc)
+int process_count(process_t *proc)
 {
 	int i = 0;
-	struct process *pointer = proc;
+	process_t *pointer = proc;
 	
 	while(pointer->next != NULL)
 	{
@@ -157,7 +178,7 @@ int process_count(struct process *proc)
 	return i;
 }
 
-int process_count_children(struct process *proc)
+int process_count_children(process_t *proc)
 {
 	if(proc == NULL || proc->child == NULL)
 	{
@@ -167,9 +188,9 @@ int process_count_children(struct process *proc)
 	return process_count(proc->child) + 1;
 }
 
-struct process *process_search_pid(struct process *proc, pid_t pid)
+process_t *process_search_pid(process_t *proc, pid_t pid)
 {
-	struct process *found_proc = NULL;
+	process_t *found_proc = NULL;
 	
 	if(proc == NULL)
 	{
@@ -204,7 +225,7 @@ struct process *process_search_pid(struct process *proc, pid_t pid)
 	return NULL;
 }
 
-int main(void)
+void process_retrieve(void)
 {
 	glibtop_proclist proclist;
 	glibtop_proc_state procstate;
@@ -212,10 +233,8 @@ int main(void)
 	pid_t *list = NULL;
 	unsigned long int i = 0;
 	
-	struct process *root_proc = NULL;
-	struct process *temp_proc = NULL;
-	struct process *found_proc = NULL;
-	struct process *last_proc = NULL;
+	process_t *temp_proc = NULL;
+	process_t *found_proc = NULL;
 	
 	glibtop_init();
 	
@@ -226,7 +245,7 @@ int main(void)
 		glibtop_get_proc_state(&procstate, list[0]);
 		glibtop_get_proc_uid(&procuid, list[0]);
 		
-		root_proc = process_new(list[0], procuid.ppid, procstate.cmd);
+		process_root = process_new(list[0], procuid.ppid, procstate.cmd);
 	}
 	
 	if(proclist.number > 1)
@@ -239,11 +258,11 @@ int main(void)
 			temp_proc = process_new(list[i], procuid.ppid, procstate.cmd);
 			if(procuid.ppid == 0)
 			{
-				process_append(process_last(root_proc), temp_proc);
+				process_append(process_last(process_root), temp_proc);
 			}
 			else
 			{
-				found_proc = process_search_pid(root_proc, procuid.ppid);
+				found_proc = process_search_pid(process_root, procuid.ppid);
 				if(found_proc != NULL)
 				{
 					if(process_count_children(found_proc) == 0)
@@ -261,9 +280,135 @@ int main(void)
 	
 	g_free(list);
 	
-	process_print(root_proc, 0);
+	process_print(process_root, 0);
+}
+
+void process_monitor_job_add(pid_t pid, process_monitor_t monitor, process_monitor_action_t action)
+{
+	glibtop_proc_state procstate;
+	glibtop_proc_uid procuid;
+	glibtop_proc_args procargs;
+	process_monitor_job_t *job_list_backup = process_monitor_job_list;
+	char **args = NULL;
 	
-	process_free(root_proc);
+	if(process_monitor_job_list == NULL)
+	{
+		process_monitor_job_list = malloc(sizeof(process_monitor_job_t));
+		
+		if(process_monitor_job_list == NULL)
+		{
+			return;
+		}
+		
+		process_monitor_job_list_amount++;
+	}
+	else
+	{
+		process_monitor_job_list = realloc(process_monitor_job_list, ++process_monitor_job_list_amount * sizeof(process_monitor_job_t));
+		
+		if(process_monitor_job_list == NULL)
+		{
+			process_monitor_job_list_amount--;
+			process_monitor_job_list = job_list_backup;
+			
+			return;
+		}
+	}
+	
+	glibtop_get_proc_state(&procstate, pid);
+	glibtop_get_proc_uid(&procuid, pid);
+	args = glibtop_get_proc_argv(&procargs, pid, 0);
+	
+	process_monitor_job_list[process_monitor_job_list_amount - 1].pid = pid;
+	process_monitor_job_list[process_monitor_job_list_amount - 1].command = strdup(procstate.cmd);
+	process_monitor_job_list[process_monitor_job_list_amount - 1].monitor = monitor;
+	process_monitor_job_list[process_monitor_job_list_amount - 1].action = action;
+	process_monitor_job_list[process_monitor_job_list_amount - 1].args = args;
+}
+
+void process_monitor_job_print(void)
+{
+	int i = 0;
+	
+	for(i = 0; i < process_monitor_job_list_amount; i++)
+	{
+		printf("%s\n", process_monitor_job_list[i].command);
+	}
+}
+
+void process_monitor_job_cleanup(void)
+{
+	int i = 0;
+	
+	for(i = 0; i < process_monitor_job_list_amount; i++)
+	{
+		free(process_monitor_job_list[i].command);
+		g_strfreev(process_monitor_job_list[i].args);
+	}
+	
+	free(process_monitor_job_list);
+}
+
+void process_monitor_job_handle(process_monitor_job_t *job)
+{
+	printf("Unexpected event: %s\n", job->command);
+}
+
+void process_monitor_job_compare(void)
+{
+	process_t *found_proc = NULL;
+	int i = 0;
+	
+	for(i = 0; i < process_monitor_job_list_amount; i++)
+	{
+		switch(process_monitor_job_list[i].monitor)
+		{
+			case TERMINATE:
+			{
+				printf("TERMINATE: %s (%i)\n", process_monitor_job_list[i].command, process_monitor_job_list[i].pid);
+				
+				found_proc = process_search_pid(process_root, process_monitor_job_list[i].pid);
+				if(found_proc == NULL)
+				{
+					process_monitor_job_handle(&(process_monitor_job_list[i]));
+					
+					break;
+				}
+				
+				if(strcmp(found_proc->command, process_monitor_job_list[i].command) != 0)
+				{
+					process_monitor_job_handle(&(process_monitor_job_list[i]));
+					
+					break;
+				}
+				
+				printf("Process exists. Everything is fine!\n");
+				
+				break;
+			}
+			default:
+			{
+				printf("Unknown compare method.\n");
+				break;
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	process_retrieve();
+	
+	process_monitor_job_add(1, TERMINATE, DO_NOTHING);
+	process_monitor_job_add(1180, TERMINATE, DO_NOTHING);
+	process_monitor_job_add(9999, TERMINATE, RESTART);
+	
+	process_monitor_job_print();
+	process_monitor_job_compare();
+	
+	process_monitor_job_cleanup();
+	
+	process_free(process_root);
 	
 	return 0;
 }
